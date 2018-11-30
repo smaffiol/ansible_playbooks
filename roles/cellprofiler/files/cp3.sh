cfc4c433b35417239024c499577880fa66291476
Sergio Maffioletti <sergio.maffioletti@uzh.ch>
2018-11-30 15:09:32 +0100

    cellprofiler 3

--- roles/cellprofiler/files/cp3.sh
+++ roles/cellprofiler/files/cp3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run cellprofiler 3 docker image
-sudo docker run --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -v /data:/data cellprofiler/cellprofiler:3.0.0 -p $@ -c -r -o /data --plugins-directory=/data
+sudo docker run --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -v /data:/data cellprofiler/cellprofiler -p $@ -c -r -o /data --plugins-directory=/data
